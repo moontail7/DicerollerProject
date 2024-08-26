c90bba1c176be28056b4bc456c3c34570c83ece7
InternetRat18 <126046170+InternetRat18@users.noreply.github.com>
2024-08-26 21:04:32 +1000

    Update module-info.java
    idek if anything changed here

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.dicerollerproject {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.desktop;
 
     opens com.example.dicerollerproject to javafx.fxml;
     exports com.example.dicerollerproject;
