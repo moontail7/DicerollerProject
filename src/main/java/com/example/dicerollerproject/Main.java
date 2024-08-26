f646de8077ae8e089e5ce230163215268fabb3d9
InternetRat18 <126046170+InternetRat18@users.noreply.github.com>
2024-08-26 21:05:45 +1000

    Update Main.java
    Identifies the screen resolution and makes window fill half the screen (and added a few comments)

--- src/main/java/com/example/dicerollerproject/Main.java
+++ src/main/java/com/example/dicerollerproject/Main.java
@@ -6,14 +6,29 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.util.Objects;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/com/example/dicerollerproject/main.fxml"));
-        primaryStage.setTitle("Dice Roller");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
+    public void start(Stage Window) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/dicerollerproject/main.fxml")));
+        //declares (and loads) the fxml file
+
+        Window.setTitle("Dice Roller");
+
+        int screenWidth = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+        int screenHeight = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+        Window.setWidth(screenWidth/2);
+        Window.setHeight(screenHeight/2);
+        //Retrieve screen dimensions and set window to half the size
+
+        Window.setScene(new Scene(root, 6, 4));
+        //ngl doesnt know what this does but its VERY important
+
+        Window.show();
+        //shows the window to the user
     }
 
     public static void main(String[] args) {
