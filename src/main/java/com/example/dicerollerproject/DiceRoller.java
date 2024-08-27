47f43fbb751d7429cd334a51bf8b8664cd74269d
InternetRat18 <126046170+InternetRat18@users.noreply.github.com>
2024-08-27 18:10:33 +1000

    Update DiceRoller.java
    Removed the roll a single d6 thingy and replaced it with 'quick inputs' so you can quickly add an extra d6 to the input arguments (among the other fundamental dice, d4, d8, d10, d12, d20)

--- src/main/java/com/example/dicerollerproject/DiceRoller.java
+++ src/main/java/com/example/dicerollerproject/DiceRoller.java
@@ -1,5 +1,6 @@
 package com.example.dicerollerproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -19,6 +20,7 @@ public class DiceRoller {
     //btn used to roll a single d6
     //^^ Initializes the interface screen elements so they can be referenced (and altered) later
 
+
     @FXML
     public void btnRollDiceClick() {
         //event
@@ -28,13 +30,70 @@ public class DiceRoller {
             lblRollText.setText("Invalid Input. Please try again (but better this time).");
         }
     }
-    //(below) test rolling a 6 sided die with button
-    public void btnRollSingleDieClick() {
-        Random random = new Random();
-        int roll = random.nextInt(6) + 1;
-        //roll a number between 1 and 6
-        lblRollText.setText("You rolled a " + roll + "!");
-        //display the roll of the d6 on the lbl
+
+    public void btnQISingleD4Click(ActionEvent actionEvent) {
+        //btn Event to add "1d4" to the tbxInput text field
+        tbxInput.setText(QIDice("4", tbxInput.getText()));
+        //Inputs/adds a d"4" to the tbx inout
+    }
+
+    public void btnQISingleD6Click(ActionEvent actionEvent) {
+        //btn Event to add "1d6" to the tbxInput text field
+        tbxInput.setText(QIDice("6", tbxInput.getText()));
+        //Inputs/adds a d"6" to the tbx inout
+    }
+
+    public void btnQISingleD8Click(ActionEvent actionEvent) {
+        //btn Event to add "1d8" to the tbxInput text field
+        tbxInput.setText(QIDice("8", tbxInput.getText()));
+        //Inputs/adds a d"8" to the tbx inout
+    }
+
+    public void btnQISingleD10Click(ActionEvent actionEvent) {
+        //btn Event to add "1d10" to the tbxInput text field
+        tbxInput.setText(QIDice("10", tbxInput.getText()));
+        //Inputs/adds a d"10" to the tbx inout
+    }
+
+    public void btnQISingleD12Click(ActionEvent actionEvent) {
+        //btn Event to add "1d12" to the tbxInput text field
+        tbxInput.setText(QIDice("12", tbxInput.getText()));
+        //Inputs/adds a d"12" to the tbx inout
+    }
+
+    public void btnQISingleD20Click(ActionEvent actionEvent) {
+        //btn Event to add "1d20" to the tbxInput text field
+        tbxInput.setText(QIDice("20", tbxInput.getText()));
+        //Inputs/adds a d"20" to the tbx inout
+    }
+
+    private  static String QIDice(String DiceSidesNum, String tbxInputText) {
+        String Output = "";
+        if (tbxInputText.contains("d" + DiceSidesNum)) {
+            String[] InputArguments = tbxInputText.split("\\+");
+            //split the tbxInout by the '+' into its individual arguments
+            for (String InputArgument : InputArguments) {
+                //cycle though each argument
+                if (InputArgument.contains("d" + DiceSidesNum)) {
+                    //retrieve the first d20 argument in the tbxInput
+                    int d20diceCount = Integer.parseInt(InputArgument.replace("d" + DiceSidesNum, "")) + 1;
+                    //isolate the dice count amount and add 1
+                    Output = tbxInputText.replaceFirst(InputArgument, d20diceCount + "d" + DiceSidesNum);
+                    //replace only the first occurrence of the d20 argument.
+                    break;
+                }
+            }
+        } else if (tbxInputText.length() == 0) {
+            //if text is empty
+            Output = tbxInputText + "1d" + DiceSidesNum;
+        } else if (tbxInputText.substring(tbxInputText.length() - 1).equals("+")) {
+            //text is not empty, and no d20 argument (but there is a "+" on the end for some reason)
+            Output = tbxInputText + "1d" + DiceSidesNum;
+        } else {
+            //text is not empty, and no d20 argument
+            Output = tbxInputText + "+1d" + DiceSidesNum;
+        }
+        return Output;
     }
 
     private static String MainDiceRoller(String input) {
