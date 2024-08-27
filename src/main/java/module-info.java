3c5e81c67b7d3c6181544fff124faf0b17fe6c92
InternetRat18 <126046170+InternetRat18@users.noreply.github.com>
2024-08-27 18:04:30 +1000

    Added an extra 'requires' to module-info.java
    I added an extra 'requires' ("requires jdk.jdi;") to module-info.java, ngl don't know why I needed the extra one (I wasn't watching this file while editing) but its there and I don't want it to break.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.dicerollerproject {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires jdk.jdi;
 
     opens com.example.dicerollerproject to javafx.fxml;
     exports com.example.dicerollerproject;
