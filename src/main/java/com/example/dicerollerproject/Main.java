57b9d1b966e02386679bf66eaff7d634fc64f2b7
InternetRat18 <126046170+InternetRat18@users.noreply.github.com>
2024-09-03 00:34:33 +1000

    Extra func To Main.java
    Added the openMainWindow() function/method to transition from the login to the main window

--- src/main/java/com/example/dicerollerproject/Main.java
+++ src/main/java/com/example/dicerollerproject/Main.java
@@ -1,5 +1,11 @@
 package com.example.dicerollerproject;
 
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Connection;
+//^not being used currently but are used for one time executions, such as creating a table
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,27 +16,22 @@ import java.awt.*;
 import java.util.Objects;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage Window) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/dicerollerproject/main.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/dicerollerproject/login.fxml")));
         //declares (and loads) the fxml file
 
         Window.setTitle("Dice Roller");
 
-        int screenWidth = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+        int screenWidth = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
         int screenHeight = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-        Window.setWidth(screenWidth/2);
-        Window.setHeight(screenHeight/2);
+        Window.setWidth(screenWidth/2.0);
+        Window.setHeight(screenHeight/2.0);
         //Retrieve screen dimensions and set window to half the size
 
         Window.setScene(new Scene(root, 600, 400));
         //ngl don't know what this does but its VERY important (breaks without it)
         //^^ Creates the window (without showing it)?
-
-        //Window.getScene().getStylesheets().add("/com/example/dicerollerproject/StyleSheet.css");
-        //^^ this does not add the stylesheet unsure why (assuming the css exists)
-        //also I spent way too much time trying to get this to work (it's a time sink)
 
         Window.show();
         //shows the window to the user
@@ -38,5 +39,23 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static void openMainWindow() throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("/com/example/dicerollerproject/main.fxml")));
+        Stage Window = new Stage();
+
+        int screenWidth = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+        int screenHeight = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+        Window.setWidth(screenWidth/2.0);
+        Window.setHeight(screenHeight/2.0);
+        //Retrieve screen dimensions and set window to half the size
+
+        Window.setScene(new Scene(root, 600, 400));
+        //ngl don't know what this does but its VERY important (breaks without it)
+        //^^ Creates the window (without showing it)?
+
+        Window.show();
+        //shows the window to the user
     }
 }
