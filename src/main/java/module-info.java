faabbb6b2530801f67fdb42c63e45f704ebb47a1
InternetRat18 <126046170+InternetRat18@users.noreply.github.com>
2024-09-03 00:33:12 +1000

    Update module-info.java
    Added requirement for sql

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.dicerollerproject {
     requires javafx.fxml;
     requires java.desktop;
     requires jdk.jdi;
+    requires java.sql;
 
     opens com.example.dicerollerproject to javafx.fxml;
     exports com.example.dicerollerproject;
