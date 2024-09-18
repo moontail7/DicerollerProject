6728dbea6561c92d6ad6f63dab1994b09709b6a2
moontail7 <e.jacob.collins@gmail.com>
2024-09-18 21:04:58 +1000

    tidied formatting

--- src/main/java/com/example/dicerollerproject/Main.java
+++ src/main/java/com/example/dicerollerproject/Main.java
@@ -42,7 +42,7 @@ public class Main extends Application {
     // Methods for showing scenes
     // stage > scene
     public static void showLoginScene() throws IOException {
-        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/com/example/dicerollerproject/login.fxml"));
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("/com/example/dicerollerproject/main.fxml"));
         Scene scene = new Scene(loader.load());
         primaryStage.setScene(scene);
         primaryStage.show();
